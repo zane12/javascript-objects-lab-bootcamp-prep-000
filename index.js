6fbf1bb514eb3e97bfac6caab71ee1d73c5bb58c
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-31 07:06:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   
   var newObject = Object.assign({}, object)
-  delete newObject.key
+  delete newObject[key]
   return newObject
 
   
 }
+
 
 
 
